9fda448bac314faae49c3e1a8fde57a858f895a2
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-18 13:52:37 +0200

    add task 01

---
+++ 01.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    window.scrollTo({
+    top: 2000,
+    behavior: "smooth"
+   })
+}, 3000);
+setInterval(() => {
+   window.scrollTo({
+   top: 0,
+   behavior: "smooth"
+   })
+}, 4000)
