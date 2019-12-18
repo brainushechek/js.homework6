3822b293a126e337984addb9027525aa0a55cb2a
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2019-12-18 14:00:10 +0200

    add task 11

---
+++ 11.js
@@ -0,0 +1,3 @@
+for ( var i = 0; i < document.getElementsByClassName('www').length; i++ ) {
+    document.getElementsByClassName('www')[i].innerHTML += document.getElementsByClassName('www')[i].tagName.toLowerCase()
+    }
